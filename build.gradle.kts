1f493f8871993a88c17f41d66a3503fd848af91f
Marek Langiewicz <marek.langiewicz@gmail.com>
2021-09-12 22:04:21 +0200

    init with falling balls example from compose-jb

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("js") version "1.5.30"
+    kotlin("multiplatform") version Vers.kotlin
+    id("org.jetbrains.compose") version Vers.composeDesktop
 }
 
 group = "pl.mareklangiewicz"
@@ -7,20 +8,68 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
+    google()
+    maven(Repos.composeDesktopDev)
+//    maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
 }
 
-dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-html:0.7.2")
-}
+//dependencies {
+//    implementation("org.jetbrains.kotlinx:kotlinx-html:0.7.2")
+//}
 
 kotlin {
+    jvm()
     js(IR) {
+        browser()
         binaries.executable()
-        browser {
-            commonWebpackConfig {
-                cssSupport.enabled = true
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+                implementation(compose.web.widgets)
+                implementation(compose.runtime)
             }
         }
+
+        val jvmMain by getting {
+            dependencies {
+                implementation(compose.desktop.currentOs)
+            }
+        }
+    }
+}
+compose.desktop {
+    application {
+        mainClass = "org.jetbrains.compose.common.demo.AppKt"
+
+        nativeDistributions {
+            targetFormats(org.jetbrains.compose.desktop.application.dsl.TargetFormat.Dmg, org.jetbrains.compose.desktop.application.dsl.TargetFormat.Msi, org.jetbrains.compose.desktop.application.dsl.TargetFormat.Deb)
+            packageName = "ImageViewer"
+            packageVersion = "1.0.0"
+
+            modules("jdk.crypto.ec")
+
+            val iconsRoot = project.file("../common/src/desktopMain/resources/images")
+            macOS {
+                iconFile.set(iconsRoot.resolve("icon-mac.icns"))
+            }
+            windows {
+                iconFile.set(iconsRoot.resolve("icon-windows.ico"))
+                menuGroup = "Compose Examples"
+                // see https://wixtoolset.org/documentation/manual/v3/howtos/general/generate_guids.html
+                upgradeUuid = "18159995-d967-4CD2-8885-77BFA97CFA9F"
+            }
+            linux {
+                iconFile.set(iconsRoot.resolve("icon-linux.png"))
+            }
+        }
+    }
+}
+
+// a temporary workaround for a bug in jsRun invocation - see https://youtrack.jetbrains.com/issue/KT-48273
+afterEvaluate {
+    extensions.configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension> {
+        versions.webpackDevServer.version = "4.0.0"
     }
 }
---
+++ src/jvmMain/kotlin/modifiers/position.kt
@@ -0,0 +1,16 @@
+@file:OptIn(ExperimentalComposeWebWidgetsApi::class)
+package org.jetbrains.compose.common.demo
+
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.common.ui.Modifier
+import org.jetbrains.compose.common.foundation.layout.offset
+import org.jetbrains.compose.common.ui.unit.Dp
+import org.jetbrains.compose.common.internal.castOrCreate
+import org.jetbrains.compose.common.ui.unit.implementation
+import androidx.compose.foundation.layout.offset
+import org.jetbrains.compose.common.ui.ExperimentalComposeWebWidgetsApi
+
+@Composable
+actual fun Modifier.position(width: Dp, height: Dp): Modifier = castOrCreate().apply {
+    modifier = modifier.offset(width.implementation, height.implementation)
+}
