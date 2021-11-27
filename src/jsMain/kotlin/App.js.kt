ab9f14ab02144dd03e69837f2365a1d68891182c
Marek Langiewicz <marek.langiewicz@gmail.com>
2021-11-28 00:08:06 +0100

    experiments

--- src/jsMain/kotlin/App.js.kt
+++ src/jsMain/kotlin/App.js.kt
@@ -13,13 +13,17 @@ import org.jetbrains.compose.web.dom.H2
 import org.jetbrains.compose.web.renderComposable
 import org.jetbrains.compose.web.ui.Styles
 import org.w3c.dom.HTMLElement
+import org.w3c.dom.events.MouseEvent
+import org.w3c.dom.events.WheelEvent
 import pl.mareklangiewicz.widgets.CmnDText
 import pl.mareklangiewicz.widgets.kim.KeyDownEffect
 import pl.mareklangiewicz.widgets.kim.Kim
 import pl.mareklangiewicz.widgets.kim.Kim.Companion.cmd
 import pl.mareklangiewicz.widgets.kim.Kim.Companion.collect
-import pl.mareklangiewicz.widgets.kim.Kim.Companion.toggle
+import pl.mareklangiewicz.widgets.kim.Kim.Companion.state
 import pl.mareklangiewicz.widgets.kim.Kim.Key
+import pl.mareklangiewicz.widgets.kim.MouseMoveEffect
+import pl.mareklangiewicz.widgets.kim.MouseWheelEffect
 
 fun main() {
     console.log("Kotlin version: ${KotlinVersion.CURRENT}")
@@ -31,6 +35,8 @@ fun main() {
     Style(Styles)
     Kim.Area {
         Kim.KeyDownEffect(window)
+        Kim.MouseMoveEffect(window)
+        Kim.MouseWheelEffect(window)
         Key("q").cmd().collect { window.close() }
         Kim.Frame { AppContent() }
     }
@@ -50,22 +56,21 @@ fun main() {
     Key("j").cmd().collect { camPosY += 0.1 }
     Key("i").cmd().collect { camPosZ -= 0.1 }
     Key("o").cmd().collect { camPosZ += 0.1 }
+    Key("mousemove").cmd().collect {
+        val evt = it.data as MouseEvent
+        if (evt.buttons.toInt() != 0) {
+            camPosX = evt.offsetX / 100 + 5
+            camPosY = evt.offsetY / 100 + 5
+        }
+    }
+    Key("wheel").cmd().collect {
+        camPosZ += (it.data as WheelEvent).deltaY.toFloat() / 100
+    }
 
-    val toggled by Key("t").toggle()
+    val toggled by Key("t").cmd(toggled = false).state { it?.toggled ?: false }
 
     H2 { Text("Kthreelhu JS ($toggled)") }
-    Div(attrs = {
-        onWheel {
-            it.preventDefault()
-            camPosZ += it.deltaY.toFloat() / 100
-        }
-        onMouseMove {
-            if (it.buttons.toInt() != 0) {
-                camPosX = - it.offsetX / 100 + 5
-                camPosY = it.offsetY / 100 - 5
-            }
-        }
-    }) {
+    Div {
         CmnDText("camera: ${camPosX.toFixed()}, ${camPosY.toFixed()}, ${camPosZ.toFixed()}", mono = true)
         CmnDText("try mouse click+move and wheel", mono = true)
         KthExamples(camPosX xy camPosY yz camPosZ)
