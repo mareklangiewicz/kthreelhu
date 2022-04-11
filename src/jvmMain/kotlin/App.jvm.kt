aa2b7a607ad364528b8007378f6c48db131126fc
Marek Langiewicz <marek.langiewicz@gmail.com>
2022-04-11 17:08:15 +0200

    fun

--- src/jvmMain/kotlin/App.jvm.kt
+++ src/jvmMain/kotlin/App.jvm.kt
@@ -1,13 +1,11 @@
 package pl.mareklangiewicz.kthreelhu
 
+import androidx.compose.material3.*
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import org.jetbrains.compose.common.material.Text
-import org.jetbrains.compose.common.ui.ExperimentalComposeWebWidgetsApi
 import pl.mareklangiewicz.kommon.cmnPlatformIsJs
 import pl.mareklangiewicz.kommon.cmnPlatformIsJvm
 
-@OptIn(ExperimentalComposeWebWidgetsApi::class)
 fun main() = application {
     check(cmnPlatformIsJvm && !cmnPlatformIsJs)
     Window(onCloseRequest = ::exitApplication, title = "Kthreelhu") {
