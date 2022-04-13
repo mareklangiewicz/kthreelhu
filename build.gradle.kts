0b98db8d421710bcc915623b5ea82b6e80c46c0e
Marek Langiewicz <marek.langiewicz@gmail.com>
2022-04-13 11:11:47 +0200

    fun

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,55 +1,35 @@
+import org.jetbrains.compose.*
+import org.jetbrains.kotlin.gradle.plugin.*
+import org.jetbrains.kotlin.gradle.dsl.*
+import pl.mareklangiewicz.defaults.*
+import pl.mareklangiewicz.deps.*
+
+
 plugins {
     kotlin("multiplatform") version vers.kotlinForCompose
     id("org.jetbrains.compose") version vers.composeJb
 }
 
-group = "pl.mareklangiewicz"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenLocal()
-    mavenCentral()
-    google()
-    maven(repos.composeDesktopDev)
+defaultBuildTemplateForComposeMppApp(
+    appMainPackage = "pl.mareklangiewicz.kthreelhu",
+    details = libs.Kthreelhu,
+    withComposeWebWidgets = true,
+) {
+    implementation(deps.kotlinxDateTime)
+    implementation(deps.kotlinxCoroutinesCore)
+    implementation(deps.upue)
 }
 
 kotlin {
-    jvm()
-    js(IR) {
-        browser {
-            webpackTask {
-//                output.libraryTarget = "var"
-            }
-        }
-        binaries.executable()
-    }
     sourceSets {
         val commonMain by getting {
             kotlin.srcDir("../kokpit667/kommon/src/commonMain/kotlin")
             kotlin.srcDir("../kokpit667/widgets/src/commonMain/kotlin")
-            dependencies {
-                implementation(deps.kotlinxDateTime)
-                implementation(deps.kotlinxCoroutinesCore)
-                implementation(compose.runtime)
-                implementation(compose.web.core)
-                implementation(compose.web.widgets)
-                implementation(deps.upue)
-            }
         }
-
         val jvmMain by getting {
             kotlin.srcDir("../kokpit667/kommon/src/jvmMain/kotlin")
             kotlin.srcDir("../kokpit667/widgets/src/jvmMain/kotlin")
-            dependencies {
-                implementation(compose.desktop.currentOs)
-                implementation(compose.ui)
-                implementation(compose.foundation)
-                @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
-                implementation(compose.material3)
-                implementation(compose.material)
-            }
         }
-
         val jsMain by getting {
             kotlin.srcDir("../kokpit667/kommon/src/jsMain/kotlin")
             kotlin.srcDir("../kokpit667/widgets/src/jsMain/kotlin")
@@ -61,14 +41,240 @@ kotlin {
         }
     }
 }
-compose.desktop {
-    application {
-        mainClass = "pl.mareklangiewicz.kthreelhu.App_jvmKt"
 
-        nativeDistributions {
-            targetFormats(org.jetbrains.compose.desktop.application.dsl.TargetFormat.Dmg, org.jetbrains.compose.desktop.application.dsl.TargetFormat.Msi, org.jetbrains.compose.desktop.application.dsl.TargetFormat.Deb)
-            packageName = "kthreelhu"
-            packageVersion = "1.0.0"
+// region [Kotlin Module Build Template]
+
+fun TaskCollection<Task>.defaultKotlinCompileOptions(
+    jvmTargetVer: String = vers.defaultJvm,
+    requiresOptIn: Boolean = true
+) = withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = jvmTargetVer
+        if (requiresOptIn) freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
+    }
+}
+
+// endregion [Kotlin Module Build Template]
+
+
+// region [MPP Module Build Template]
+
+/** Only for very standard small libs. In most cases it's better to not use this function. */
+fun Project.defaultBuildTemplateForMppLib(
+    details: LibDetails = libs.Unknown,
+    withJvm: Boolean = true,
+    withJs: Boolean = true,
+    withNativeLinux64: Boolean = false,
+    withKotlinxHtml: Boolean = false,
+    withComposeJbDevRepo: Boolean = false,
+    addCommonMainDependencies: KotlinDependencyHandler.() -> Unit = {}
+) {
+    repositories { defaultRepos(withKotlinxHtml = withKotlinxHtml, withComposeJbDev = withComposeJbDevRepo) }
+    defaultGroupAndVerAndDescription(details)
+    kotlin { allDefault(withJvm, withJs, withNativeLinux64, withKotlinxHtml, addCommonMainDependencies) }
+    tasks.defaultKotlinCompileOptions()
+    tasks.defaultTestsOptions()
+    if (plugins.hasPlugin("maven-publish")) {
+        defaultPublishing(details)
+        if (plugins.hasPlugin("signing")) defaultSigning()
+        else println("MPP Module ${name}: signing disabled")
+    }
+    else println("MPP Module ${name}: publishing (and signing) disabled")
+}
+
+/** Only for very standard small libs. In most cases it's better to not use this function. */
+@Suppress("UNUSED_VARIABLE")
+fun KotlinMultiplatformExtension.allDefault(
+    withJvm: Boolean = true,
+    withJs: Boolean = true,
+    withNativeLinux64: Boolean = false,
+    withKotlinxHtml: Boolean = false,
+    addCommonMainDependencies: KotlinDependencyHandler.() -> Unit = {}
+) {
+    if (withJvm) jvm()
+    if (withJs) jsDefault()
+    if (withNativeLinux64) linuxX64()
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                if (withKotlinxHtml) implementation(deps.kotlinxHtml)
+                addCommonMainDependencies()
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation(deps.uspekx)
+            }
         }
     }
 }
+
+
+fun KotlinMultiplatformExtension.jsDefault(
+    withBrowser: Boolean = true,
+    withNode: Boolean = false,
+    testWithChrome: Boolean = true,
+    testHeadless: Boolean = true,
+) {
+    js(IR) {
+        if (withBrowser) browser {
+            testTask {
+                useKarma {
+                    when (testWithChrome to testHeadless) {
+                        true to true -> useChromeHeadless()
+                        true to false -> useChrome()
+                    }
+                }
+            }
+        }
+        if (withNode) nodejs()
+    }
+}
+
+// endregion [MPP Module Build Template]
+
+// region [Compose MPP Module Build Template]
+
+/** Only for very standard compose mpp libs. In most cases it's better to not use this function. */
+@Suppress("UNUSED_VARIABLE")
+@OptIn(ExperimentalComposeLibrary::class)
+fun Project.defaultBuildTemplateForComposeMppLib(
+    details: LibDetails = libs.Unknown,
+    withJvm: Boolean = true,
+    withJs: Boolean = true,
+    withNativeLinux64: Boolean = false,
+    withKotlinxHtml: Boolean = false,
+    withComposeUi: Boolean = withJvm,
+    withComposeFoundation: Boolean = withJvm,
+    withComposeMaterial2: Boolean = withJvm,
+    withComposeMaterial3: Boolean = withJvm,
+    withComposeMaterialIconsExtended: Boolean = withJvm,
+    withComposeFullAnimation: Boolean = withJvm,
+    withComposeDesktop: Boolean = withJvm,
+    withComposeDesktopComponents: Boolean = withJvm,
+    withComposeWebCore: Boolean = withJs,
+    withComposeWebWidgets: Boolean = false,
+    withComposeWebSvg: Boolean = withJs,
+    withComposeTestUiJUnit4: Boolean = withJvm,
+    withComposeTestWebUtils: Boolean = withJs,
+    addCommonMainDependencies: KotlinDependencyHandler.() -> Unit = {}
+) {
+    defaultBuildTemplateForMppLib(details, withJvm, withJs, withNativeLinux64, withKotlinxHtml, true, addCommonMainDependencies)
+    kotlin {
+        sourceSets {
+            val commonMain by getting {
+                dependencies {
+                    implementation(compose.runtime)
+                    if (withComposeWebCore) implementation(compose.web.core)
+                    @Suppress("DEPRECATION")
+                    if (withComposeWebWidgets) implementation(compose.web.widgets)
+                }
+            }
+            val jvmMain by getting {
+                dependencies {
+                    if (withComposeUi) {
+                        implementation(compose.ui)
+                        implementation(compose.uiTooling)
+                        implementation(compose.preview)
+                    }
+                    if (withComposeFoundation) implementation(compose.foundation)
+                    if (withComposeMaterial2) implementation(compose.material)
+                    if (withComposeMaterial3) implementation(compose.material3)
+                    if (withComposeMaterialIconsExtended) implementation(compose.materialIconsExtended)
+                    if (withComposeFullAnimation) {
+                        implementation(compose.animation)
+                        implementation(compose.animationGraphics)
+                    }
+                    if (withComposeDesktop) {
+                        implementation(compose.desktop.common)
+                        implementation(compose.desktop.currentOs)
+                    }
+                    if (withComposeDesktopComponents) {
+                        implementation(compose.desktop.components.splitPane)
+                    }
+                }
+            }
+            val jsMain by getting {
+                dependencies {
+                    implementation(compose.runtime)
+                    if (withComposeWebSvg) implementation(compose.web.svg)
+                }
+            }
+            val jvmTest by getting {
+                dependencies {
+                    if (withComposeTestUiJUnit4) implementation(compose.uiTestJUnit4)
+                }
+            }
+            val jsTest by getting {
+                dependencies {
+                    if (withComposeTestWebUtils) implementation(compose.web.testUtils)
+                }
+            }
+        }
+    }
+}
+
+// endregion [Compose MPP Module Build Template]
+
+// region [Compose MPP App Build Template]
+
+/** Only for very standard compose mpp apps. In most cases it's better to not use this function. */
+@Suppress("UNUSED_VARIABLE")
+fun Project.defaultBuildTemplateForComposeMppApp(
+    appMainPackage: String,
+    appMainClass: String = "App_jvmKt", // for compose jvm
+    appMainFun: String = "main", // for native
+    details: LibDetails = libs.Unknown,
+    withJvm: Boolean = true,
+    withJs: Boolean = true,
+    withNativeLinux64: Boolean = false,
+    withKotlinxHtml: Boolean = false,
+    withComposeUi: Boolean = withJvm,
+    withComposeFoundation: Boolean = withJvm,
+    withComposeMaterial2: Boolean = withJvm,
+    withComposeMaterial3: Boolean = withJvm,
+    withComposeMaterialIconsExtended: Boolean = withJvm,
+    withComposeFullAnimation: Boolean = withJvm,
+    withComposeDesktop: Boolean = withJvm,
+    withComposeDesktopComponents: Boolean = withJvm,
+    withComposeWebCore: Boolean = withJs,
+    withComposeWebWidgets: Boolean = false,
+    withComposeWebSvg: Boolean = withJs,
+    withComposeTestUiJUnit4: Boolean = withJvm,
+    withComposeTestWebUtils: Boolean = withJs,
+    addCommonMainDependencies: KotlinDependencyHandler.() -> Unit = {}
+) {
+    defaultBuildTemplateForComposeMppLib(details, withJvm, withJs, withNativeLinux64, withKotlinxHtml, withComposeUi,
+        withComposeFoundation, withComposeMaterial2, withComposeMaterial3, withComposeMaterialIconsExtended,
+        withComposeFullAnimation, withComposeDesktop, withComposeDesktopComponents, withComposeWebCore,
+        withComposeWebWidgets, withComposeWebSvg, withComposeTestUiJUnit4, withComposeTestWebUtils,
+        addCommonMainDependencies)
+    kotlin {
+        if (withJs) js(IR) {
+            binaries.executable()
+        }
+        if (withNativeLinux64) linuxX64 {
+            binaries {
+                executable {
+                    entryPoint = "$appMainPackage.$appMainFun"
+                }
+            }
+        }
+    }
+    if (withJvm) {
+        compose.desktop {
+            application {
+                mainClass = "$appMainPackage.$appMainClass"
+                nativeDistributions {
+                    targetFormats(org.jetbrains.compose.desktop.application.dsl.TargetFormat.Deb)
+                    packageName = details.name
+                    packageVersion = details.version
+                    description = details.description
+                }
+            }
+        }
+    }
+}
+
+// endregion [Compose MPP App Build Template]
